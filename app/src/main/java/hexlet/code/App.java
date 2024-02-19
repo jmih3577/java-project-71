847905fa282681f79c109169e291eb63ada3850d
a d <ad@gmail.com>
2024-02-19 22:34:47 +0300

    ed

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,26 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @Parameters(index = "0", description = "path to first file")
+    private String filePath1;
+    @Parameters(index = "1", description = "path to second file")
+    private String filePath2;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.exit(new CommandLine(new App()).execute(args));
     }
 
 }
