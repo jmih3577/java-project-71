2b25ab53bf8215a409aef589bb7ade79252d34ab
a d <ad@gmail.com>
2024-02-26 21:57:43 +0300

    ed

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -17,6 +17,8 @@ public class App implements Callable<Integer> {
     private String format = "stylish";
     @Override
     public Integer call() throws Exception {
+        var result = Differ.generate(filePath1, filePath2);
+        System.out.println(result);
         return 0;
     }
     public static void main(String[] args) {
