6d8c3f8b147bb933fd232d54488472eef30e0e27
a d <ad@gmail.com>
2024-03-03 22:30:16 +0300

    eddd

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -9,7 +9,7 @@ class AppTest {
 
     @BeforeEach
     @Test
-    public void bothAbsolutePath() throws Exception {
+    public void bothAbsolutePathJSON() throws Exception {
         var expect = """
                 {
                   -follow: false
@@ -25,7 +25,8 @@ class AppTest {
         assertEquals(expect,
                 actual);
     }
-    public void bothRelativePath() throws Exception {
+    @Test
+    public void bothRelativePathJSON() throws Exception {
         var expect = """
                 {
                   -follow: false
@@ -35,8 +36,42 @@ class AppTest {
                   +timeout: 20
                   +verbose: true
                 }""";
-        var actual = Differ.generate("../../../jsons/json1.json",
-                "../../../jsons/json2.json");
+        var actual = Differ.generate("./src/test/resources/yaml1.yml",
+                "./src/test/resources/yaml2.yml");
+
+        assertEquals(expect,
+                actual);
+    }
+    @Test
+    public void bothAbsolutePathYAML() throws Exception {
+        var expect = """
+                {
+                  -follow: false
+                  host: hexlet.io
+                  -proxy: 123.234.53.22
+                  -timeout: 50
+                  +timeout: 20
+                  +verbose: true
+                }""";
+        var actual = Differ.generate("/home/dev/projects/java-project-71/app/src/test/resources/json1.json",
+                "/home/dev/projects/java-project-71/app/src/test/resources/json2.json");
+
+        assertEquals(expect,
+                actual);
+    }
+    @Test
+    public void bothRelativePathYAML() throws Exception {
+        var expect = """
+                {
+                  -follow: false
+                  host: hexlet.io
+                  -proxy: 123.234.53.22
+                  -timeout: 50
+                  +timeout: 20
+                  +verbose: true
+                }""";
+        var actual = Differ.generate("./src/test/resources/yaml1.yml",
+                "./src/test/resources/yaml2.yml");
 
         assertEquals(expect,
                 actual);
