c00fdbabd74c7be48e4c45010c93d6c2b578a030
a d <ad@gmail.com>
2024-03-13 23:21:45 +0300

    edddd

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -16,29 +16,29 @@ class AppTest {
     public void bothAbsolutePathJSONDefault() throws Exception {
         var expect = """
                 {
-                  chars1: [a, b, c]
-                  -chars2: [d, e, f]
-                  +chars2: false
-                  -checked: false
-                  +checked: true
-                  -default: null
-                  +default: [value1, value2]
-                  -id: 45
-                  +id: null
-                  -key1: value1
-                  +key2: value2
-                  numbers1: [1, 2, 3, 4]
-                  -numbers2: [2, 3, 4, 5]
-                  +numbers2: [22, 33, 44, 55]
-                  -numbers3: [3, 4, 5]
-                  +numbers4: [4, 5, 6]
-                  +obj1: {nestedKey=value, isNested=true}
-                  -setting1: Some value
-                  +setting1: Another value
-                  -setting2: 200
-                  +setting2: 300
-                  -setting3: true
-                  +setting3: none
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
         var actual = Differ.generate(System.getProperty("user.dir") + "/src/test/resources/complexjson1.json",
                 System.getProperty("user.dir") + "/src/test/resources/complexjson2.json", "stylish");
@@ -50,29 +50,29 @@ class AppTest {
     public void bothRelativePathJSONDefault() throws Exception {
         var expect = """
                 {
-                  chars1: [a, b, c]
-                  -chars2: [d, e, f]
-                  +chars2: false
-                  -checked: false
-                  +checked: true
-                  -default: null
-                  +default: [value1, value2]
-                  -id: 45
-                  +id: null
-                  -key1: value1
-                  +key2: value2
-                  numbers1: [1, 2, 3, 4]
-                  -numbers2: [2, 3, 4, 5]
-                  +numbers2: [22, 33, 44, 55]
-                  -numbers3: [3, 4, 5]
-                  +numbers4: [4, 5, 6]
-                  +obj1: {nestedKey=value, isNested=true}
-                  -setting1: Some value
-                  +setting1: Another value
-                  -setting2: 200
-                  +setting2: 300
-                  -setting3: true
-                  +setting3: none
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
         var actual = Differ.generate(System.getProperty("user.dir") + "/src/test/resources/complexjson1.json",
                 System.getProperty("user.dir") + "/src/test/resources/complexjson2.json", "stylish");
@@ -84,29 +84,29 @@ class AppTest {
     public void bothAbsolutePathYAMLDefault() throws Exception {
         var expect = """
                 {
-                  chars1: [a, b, c]
-                  -chars2: [d, e, f]
-                  +chars2: false
-                  -checked: false
-                  +checked: true
-                  -default: null
-                  +default: [value1, value2]
-                  -id: 45
-                  +id: null
-                  -key1: value1
-                  +key2: value2
-                  numbers1: [1, 2, 3, 4]
-                  -numbers2: [2, 3, 4, 5]
-                  +numbers2: [22, 33, 44, 55]
-                  -numbers3: [3, 4, 5]
-                  +numbers4: [4, 5, 6]
-                  +obj1: {nestedKey=value, isNested=true}
-                  -setting1: Some value
-                  +setting1: Another value
-                  -setting2: 200
-                  +setting2: 300
-                  -setting3: true
-                  +setting3: none
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
         var actual = Differ.generate(System.getProperty("user.dir") + "/src/test/resources/complexyaml1.yml",
                 System.getProperty("user.dir") + "/src/test/resources/complexyaml2.yml", "stylish");
@@ -118,29 +118,29 @@ class AppTest {
     public void bothRelativePathYAMLDefault() throws Exception {
         var expect = """
                 {
-                  chars1: [a, b, c]
-                  -chars2: [d, e, f]
-                  +chars2: false
-                  -checked: false
-                  +checked: true
-                  -default: null
-                  +default: [value1, value2]
-                  -id: 45
-                  +id: null
-                  -key1: value1
-                  +key2: value2
-                  numbers1: [1, 2, 3, 4]
-                  -numbers2: [2, 3, 4, 5]
-                  +numbers2: [22, 33, 44, 55]
-                  -numbers3: [3, 4, 5]
-                  +numbers4: [4, 5, 6]
-                  +obj1: {nestedKey=value, isNested=true}
-                  -setting1: Some value
-                  +setting1: Another value
-                  -setting2: 200
-                  +setting2: 300
-                  -setting3: true
-                  +setting3: none
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
                 }""";
         var actual = Differ.generate(System.getProperty("user.dir") + "/src/test/resources/complexyaml1.yml",
                 System.getProperty("user.dir") + "/src/test/resources/complexyaml2.yml", "stylish");
@@ -155,10 +155,10 @@ class AppTest {
                     Property 'checked' was updated. From false to true
                     Property 'default' was updated. From null to [complex value]
                     Property 'id' was updated. From 45 to null
-                    Property 'key1' was removed.
+                    Property 'key1' was removed
                     Property 'key2' was added with value: 'value2'
                     Property 'numbers2' was updated. From [complex value] to [complex value]
-                    Property 'numbers3' was removed.
+                    Property 'numbers3' was removed
                     Property 'numbers4' was added with value: [complex value]
                     Property 'obj1' was added with value: [complex value]
                     Property 'setting1' was updated. From 'Some value' to 'Another value'
@@ -177,10 +177,10 @@ class AppTest {
                     Property 'checked' was updated. From false to true
                     Property 'default' was updated. From null to [complex value]
                     Property 'id' was updated. From 45 to null
-                    Property 'key1' was removed.
+                    Property 'key1' was removed
                     Property 'key2' was added with value: 'value2'
                     Property 'numbers2' was updated. From [complex value] to [complex value]
-                    Property 'numbers3' was removed.
+                    Property 'numbers3' was removed
                     Property 'numbers4' was added with value: [complex value]
                     Property 'obj1' was added with value: [complex value]
                     Property 'setting1' was updated. From 'Some value' to 'Another value'
